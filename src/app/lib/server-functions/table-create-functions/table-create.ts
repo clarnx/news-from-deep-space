2b79a7decd843404bd3584b0dd465f6e8a2234f3
CrumMan <122419541+CrumMan@users.noreply.github.com>
2026-05-18 18:50:27 -0500

    ISSUE AMMENDED FOR TYPE ON COMBINED KEY WORDS.

--- src/app/lib/server-functions/table-create-functions/table-create.ts
+++ src/app/lib/server-functions/table-create-functions/table-create.ts
@@ -22,7 +22,7 @@ async function seedCombinedKeywords(){
     fk_keyword1 UUID NOT NULL,
     fk_keyword2 UUID NOT NULL,
     
-    type TEXT,
+    type TEXT NOT NULL,
     complecated_api_type TEXT NOT NULL, 
     link TEXT NOT NULL,
     complecated_link TEXT,
